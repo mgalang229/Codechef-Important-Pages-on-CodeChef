9dabba1c6b40ac957ffa32cbc799a4f441511d09
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-04-03 23:57:58 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        String ans = "";
+        if(a == 1 && b == 1) {
+            ans = "https://discuss.codechef.com";
+        } else if(a == 1) {
+            ans = "https://www.codechef.com/contests";
+        } else {
+            ans = "https://www.codechef.com/practice";
+        }
+        System.out.println(ans);
+    }
+}
