0e30d757cf7b308d5f84081c75460899dc131253
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-04-04 00:03:25 +0800

    Update main.java

--- main.java
+++ main.java
@@ -3,17 +3,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in
+                                 
         int a = in.nextInt();
         int b = in.nextInt();
+                                 
         String ans = "";
-        if(a == 1 && b == 1) {
+                                 
+        if (a == 1 && b == 1) {
             ans = "https://discuss.codechef.com";
-        } else if(a == 1) {
+        } else if (a == 1) {
             ans = "https://www.codechef.com/contests";
         } else {
             ans = "https://www.codechef.com/practice";
         }
+                                 
         System.out.println(ans);
     }
 }
